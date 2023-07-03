0da381148085ab21506a6fba7d9165fad2257eca
daslastic <daslastic@gmail.com>
2023-07-03 15:00:55 -0400

    the purge

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,80 +1,124 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 if set -q SSH_TTY
   set -g fish_color_host brred
 end
 
+# function fish_greeting
+# 	sh $(find $FISH_CONFIG/colorscripts -type f | shuf -n1)
+# end
+
 set -U fish_greeting
 
-fish_add_path /opt/homebrew/bin
-fish_vi_key_bindings
-
-alias vim "nvim"
-alias vi "nvim"
-alias c "cd"
-alias nvim "nvim"
-alias ed "echo you need help"
-alias ls "l"
+abbr vim "lvim"
+abbr vi "lvim"
+abbr v "lvim"
+abbr c "cd"
+abbr nvim "lvim"
+abbr ed "echo you need help"
+abbr ls "l"
 alias l "lsd -lA --config-file ~/.config/lsd/config.yml"
 alias lsc "ls --color=auto"
 alias dc "sl"
-alias cp "cp -ivr"
-alias scp "scp -r"
-alias mkdir "mkdir -r"
-alias rm "rm -vI"
-alias mv "mv -iv"
-alias doas="sudo"
+abbr cp "cp -ivr"
+abbr scp "scp -r"
+abbr rm "rm -vI"
+abbr mv "mv -iv"
+abbr doas "sudo"
+alias tree "tree -C"
+abbr mkdir "mkdir -p"
 alias grep "grep --color=auto"
-alias mkdir "mkdir -p"
-alias ipa=" ifconfig en0 | grep 'inet\ ' | awk '{print $2}'"
+alias ipa " ifconfig en0 | grep 'inet\ ' | awk '{print $2}'"
 alias diff "diff --color=auto"
 alias ip "ip --color=auto"
-alias wget "wget --hsts-file /tmp"
-alias upgrade="brew upgrade --casks --greedy && brew update && brew cu --all && brew bundle  --file ~/.config/homebrew/Brewfile"
-
-set -x DEVKIT "$HOME/.local/share/devkit"
-set -x EDITOR "nvim"
+alias wget "wget --hsts-file='$XDG_DATA_HOME/wget-hsts'"
+alias upgrade "brew upgrade --casks --greedy; brew upgrade; brew cu --all; brew bundle  --file ~/.config/homebrew/Brewfile"
+alias colorscript 'sh $(find $FISH_CONFIG/colorscripts -type f | shuf -n1)'
+alias p "cd $DEV/projects"
 
 # path
-set -x CACHE_HOME "$HOME/.cache"
-set -x CONFIG_HOME "$HOME/.config"
-set -x DATA_HOME "$HOME/.local/share"
-set -x BIN_HOME "$HOME/.local/bin"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x XDG_STATE_HOME "$HOME/.cache/state"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDH_BIN_HOME "$HOME/.local/bin"
+
+# global
+set -x FISH_CONFIG "$XDG_CONFIG_HOME/fish/"
+set -x EDITOR "lvim"
+set -x TERMINAL "alacritty"
+set -x TLDR_OS "macos"
 
 # zsh
-set -x HISTFILE $CACHE/.bash_history
+set -x HISTFILE $CACHE/bash_history
 set -x HISTCONTROL ignoreboth
 set -x LESSHISTFILE "-"
 
 # programs
-set PATH $PATH $HOME/.local/bin
-set PATH $PATH $DATA_HOME/npm/bin
-set PATH $PATH /opt/homebrew/bin
-
-set -x NPM_CONFIG_PREFIX "$DATA_HOME/npm"
-set -x NPM_CONFIG_USERCONFIG "$CONFIG_HOME/npm/config"
-set -x RUSTUP_HOME "$DATA_HOME/rustup"
-set -x CARGO_HOME "$DATA_HOME/cargo"
-set -x PATH "$CARGO_HOME/bin:$PATH"
-set -x GOPATH "$DATA_HOME"/go
+set -x HOMEBREW_PREFIX "/opt/homebrew"
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_INSECURE_REDIRECT 1
+set -x JAVA_HOME $(/usr/libexec/java_home -v 17.0.7)
+set -x JAVA_PREFIX $(/usr/libexec/java_home -v 17.0.7)
+set -x GRADLE_USER_HOME "$XDG_DATA_HOME"/gradle
+fish_add_path $HOMEBREW_PREFIX
+fish_add_path $HOMEBREW_PREFIX/sbin
+fish_add_path $HOMEBREW_PREFIX/opt/openssl@3/bin
+fish_add_path $JAVA_HOME/bin
+fish_vi_key_bindings
 
-# other
-set -x PYTHONSTARTUP $CONFIG_HOME/python/pythonrc
-set -x ANDROID_SDK_HOME "$CONFIG_HOME/android"
+# dev
+fish_add_path $XDH_BIN_HOME 
+fish_add_path $XDG_DATA_HOME/npm/bin
+set -x NPM_CONFIG_PREFIX "$XDG_DATA_HOME/npm"
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
+set -x RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
+set -x PATH "$CARGO_HOME/bin:$PATH"
+set -x GOPATH "$XDG_DATA_HOME"/go
+set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+set -x ANDROID_HOME "$XDG_DATA_HOME"/android
+set -x ANDROID_SDK_HOME "$XDG_CONFIG_HOME/android"
+set -x DEV "$HOME/Development"
+set -x MINECRAFT_DEV "$DEV/projects/minecraft/"
+set -x DEVKIT "$DEV/kit"
 
-mkdir -p "$DATA_HOME/rustup"
-mkdir -p "$CONFIG_HOME/android"
-mkdir -p "$DATA_HOME/cargo"
+mkdir -p "$XDG_DATA_HOME/rustup"
+mkdir -p "$XDG_CONFIG_HOME/android"
+mkdir -p "$XDG_DATA_HOME/cargo"
 
 direnv hook fish | source
-starship init fish | source
 
-source ~/.config/fish/iterm2_shell_integration.fish
+# openssl
+set -x OPENSSL_PREFIX "openssl@3.1"
+fish_add_path $HOMEBREW_PREFIX/opt/$OPENSSL_PREFIX/bin
+set -x LDFLAGS "-L/$HOMEBREW_PREFIX/opt/$OPENSSL_PREFIX/lib"
+set -x CPPFLAGS "-I/$HOMEBREW_PREFIX/opt/$OPENSSL_PREFIX/include"
+set -x PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/$OPENSSL_PREFIX/lib/pkgconfig"
 
-set -gx PNPM_HOME "$HOME/.config/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+# random plugin stuff
+set -U __done_notify_sound 1
+
+set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --height=100%'
+set fzf_directory_opts "--no-preview"
+set fzf_fd_opts . $HOME --color=never
+fzf_configure_bindings --git_status=\cg --history=\cr --variables=\cv --processes= --directory=\cs
+
+# prompt
+set -x fish_color_cwd "green"
+function fish_prompt -d "Write out the prompt"
+  printf '%s%s%s\n%s🍺 ' \
+    (set_color $fish_color_cwd) (prompt_pwd) (set_color "red"; fish_git_prompt) (set_color normal)
 end
+
+function __add_newline_after_command --on-event fish_postexec
+	if test "$argv[1]" != "clear"
+		printf "\n"
+	end
+end
+
+# lunarvim
+set -x LUNARVIM_RUNTIME_DIR $HOME/.local/share/lunarvim
+set -x LUNARVIM_CONFIG_DIR $HOME/.config/lvim
+set -x LUNARVIM_CACHE_DIR $HOME/.cache/lvim
+set -x LUNARVIM_BASE_DIR $HOME/.local/share/lunarvim/lvim
--- preferences/.config/preferences/apply.sh
+++ preferences/.config/preferences/apply.sh
@@ -1,62 +1,28 @@
-#/bin/sh
+#!/bin/sh
 
-# Close open System Preferences panes, to prevent them from overriding settings.
 osascript -e 'tell application "System Preferences" to quit'
 
 # dock
-defaults write com.apple.dock autohide -bool true       # Autohide dock.
-defaults write com.apple.dock tilesize -int 48          # Set dock icon size.
-defaults write com.apple.dock magnification -bool true  # Enable dock magnification.
-defaults write com.apple.dock largesize -int 83         # Set dock magnificated icon size.
-defaults write NSGlobalDomain AppleInterfaceStyle Dark  # Use dark menu bar and dock.
-defaults write com.apple.dock persistent-apps -array    # Delete all apps from dock.
 defaults write com.apple.dock show-recents -bool false
-defaults write -g AppleDisplayScaleFactor 0.75
-defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -float 0.45;
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.45
 killall Dock
-
-# Expand print panel by default.
-defaults write NSGlobalDomain NSNavPanelExpandedStateForPrintMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForPrintMode2 -bool true
-
-# Expand save panel by default.
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-# Keyboard
-defaults write NSGlobalDomain KeyRepeat -int 3         # Set fast key repeat rate.
-defaults write NSGlobalDomain InitialKeyRepeat -int 12  # Set fast initial key repeat rate.
 
 # Disable the "Are you sure you want to open this application?" dialog.
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Always open new documents in tabs.
-defaults write NSGlobalDomain AppleWindowTabbingMode always
-
 # Text correction
-defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false      # Disable automatic capitalization.
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false # Disable automatic capitalization.
 
 # Finder
-defaults write com.apple.finder AppleShowAllFiles -bool true                 # Show hidden files.
-defaults write NSGlobalDomain AppleShowAllExtensions -bool false              # Show all file extensions.
-defaults write com.apple.finder FXEnableExtensionsChangeWarning -bool false  # Disable file extension change warning.
-defaults write com.apple.finder ShowStatusBar -bool true                     # Show status bar.
-defaults write com.apple.finder ShowPathbar -bool true                       # Show path bar.
-defaults write com.apple.finder ShowRecentTags -bool false                   # Hide tags in sidebar.
-defaults write com.apple.finder QuitMenuItem -bool true                      # Allow quitting finder via ⌘ + Q.
-defaults write com.apple.finder SidebarWidth -int 120                        # less sidebar width.
-defaults write com.apple.finder WarnOnEmptyTrash -bool false                 # No warning before emptying trash.
-defaults write com.apple.finder FXPreferredViewStyle clmv
+defaults write com.apple.finder AppleShowAllFiles -bool true # Show hidden files.
 
 killall Finder
-
-# Automatically quit printer app once the print jobs complete.
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 # Prevent Photos from opening automatically when devices are plugged in.
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 # Stop iTunes from responding to the keyboard media keys.
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null
 # disable keychain
 defaults write com.apple.keychainaccess ShowPrefsToolbar -bool false
 
@@ -66,3 +32,4 @@ brew services start skhd
 sudo defaults write “/Library/Preferences/com.apple.mail” EnableBundles 1
 defaults write com.apple.mail EnableBundles -bool true
 defaults write com.apple.mail BundleCompatibilityVersion 4
+defaults write -g ApplePressAndHoldEnabled 0
